180bc7c86880689bec8f10a9e3bba3027aa21c59
flavio <flavioandrade0201@gmail.com>
2021-09-09 19:46:21 +0300

    Merge sort implemented

--- src/components/SortingVisualizer/SortingVisualizer.jsx
+++ src/components/SortingVisualizer/SortingVisualizer.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import "./SortingVisualizer.css";
+import { getMergeSortAnimations } from "../../Algorithms/MergeSort";
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = "turquoise";
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = "red";
 
 function SortingVisualizer() {
-  var [array, setArray] = useState([700, 850, 900]);
+  var [array, setArray] = useState([]);
 
   //function to generate random numbers between two given variables
   const randomIntFromInterval = (min, max) => {
@@ -12,33 +25,63 @@ function SortingVisualizer() {
   function resetArray() {
     setArray(() => {
       let array = [];
-      for (let i = 0; i < 200; i++) {
-        array.push(randomIntFromInterval(5, 500));
+      for (let i = 0; i < 400; i++) {
+        array.push(randomIntFromInterval(5, 700));
       }
       return array;
     });
   }
 
+  function mergeSort() {
+    const animations = getMergeSortAnimations(array);
+
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array-bar");
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+  }
+
   console.log(array);
 
   return (
-    <div className="main-container">
+    <div className="array-container">
       <div>
         <ul>
           <li>
-            <a href="#home">Merge Sort</a>
+            <a>Merge Sort</a>
           </li>
           <li>
-            <a href="#news">Quick Sort</a>
+            <a>Quick Sort</a>
           </li>
           <li>
-            <a href="#contact">Bubble Sort</a>
+            <a>Bubble Sort</a>
           </li>
           <button onClick={resetArray}>generate</button>
+          <button onClick={mergeSort}>generate</button>
         </ul>
       </div>
       {array.map((val, key) => (
-        <div className="bars" key={key} style={{ height: `${val}px` }}></div>
+        <div
+          className="array-bar"
+          key={key}
+          style={{ height: `${val}px` }}
+        ></div>
       ))}
     </div>
   );
