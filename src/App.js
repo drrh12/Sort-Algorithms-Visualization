5fa7c661642f0472d6077265f3002ae3a3d5e4a7
flavio <flavioandrade0201@gmail.com>
2021-09-09 19:10:29 +0300

    Function added/fixed that generates a new array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import SortingVisualizer from "./components/SortingVisualizer/SortingVisualizer.jsx";
+import Menu from "./components/Menu/Menu.jsx";
 
 function App() {
   return (
--- src/components/SortingVisualizer/SortingVisualizer.jsx
+++ src/components/SortingVisualizer/SortingVisualizer.jsx
@@ -2,21 +2,41 @@ import React, { useState } from "react";
 import "./SortingVisualizer.css";
 
 function SortingVisualizer() {
+  var [array, setArray] = useState([700, 850, 900]);
+
   //function to generate random numbers between two given variables
   const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-  const [array] = useState([]);
-
-  for (let i = 0; i < 200; i++) {
-    array.push(randomIntFromInterval(5, 500));
+  function resetArray() {
+    setArray(() => {
+      let array = [];
+      for (let i = 0; i < 200; i++) {
+        array.push(randomIntFromInterval(5, 500));
+      }
+      return array;
+    });
   }
 
   console.log(array);
 
   return (
     <div className="main-container">
+      <div>
+        <ul>
+          <li>
+            <a href="#home">Merge Sort</a>
+          </li>
+          <li>
+            <a href="#news">Quick Sort</a>
+          </li>
+          <li>
+            <a href="#contact">Bubble Sort</a>
+          </li>
+          <button onClick={resetArray}>generate</button>
+        </ul>
+      </div>
       {array.map((val, key) => (
         <div className="bars" key={key} style={{ height: `${val}px` }}></div>
       ))}
